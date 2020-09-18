5a5924fbb2bec673109fc6fbcea8dfedab46c873
LordMilan <milan.mahat.739@gmail.com>
2020-09-19 00:18:24 +0545

    Updates

---
+++ precache-manifest.a61185b360f11cdbd0013afc88da969f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376ab6d1c54cccc108d19e57ed58bf58",
+    "url": "/VAPECITY-NEW/index.html"
+  },
+  {
+    "revision": "03c0baba444d125796de",
+    "url": "/VAPECITY-NEW/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "51c46e6e864be16958bf",
+    "url": "/VAPECITY-NEW/static/js/2.331304c2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/VAPECITY-NEW/static/js/2.331304c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c0baba444d125796de",
+    "url": "/VAPECITY-NEW/static/js/main.8875c840.chunk.js"
+  },
+  {
+    "revision": "84a3b346c2490fcf9f2d",
+    "url": "/VAPECITY-NEW/static/js/runtime-main.5a080076.js"
+  }
+]);
